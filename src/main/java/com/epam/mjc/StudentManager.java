f144200db51fe16a5ac890c314a8ba77dee3e31a
Bilbo3340 <vladik.elce1@gmail.com>
2022-09-12 23:28:06 +0300

    upt

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,6 +6,10 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
+    if (Student.getValueOf(studentID) == null) {
+      CustomException e = new CustomException("Could not find student with ID " + studentID);
+      throw e;
+    }
     return Student.getValueOf(studentID);
   }
 
@@ -16,6 +20,5 @@ public class StudentManager {
       Student student = manager.find(IDs[i]);
       System.out.println("Student name " + student.getName());
     }
-
   }
 }
